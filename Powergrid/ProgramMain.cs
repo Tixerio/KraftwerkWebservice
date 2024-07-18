6678377023a50c0b02f2e23b721547555bfa5049
Fabian Praxmarer <fapraxmarer23@gmail.com>
2024-07-18 14:01:13 +0200

    Added Endpoints for Charts | Deleted Requester

--- Powergrid/ProgramMain.cs
+++ Powergrid/ProgramMain.cs
@@ -17,17 +17,6 @@ builder.Services.AddSwaggerGen();
 // Register the Grid as a singleton
 builder.Services.AddSingleton<Grid>();
 
-// Register the HttpClient for GridRequester
-builder.Services.AddHttpClient<IGridRequester, GridRequester>(client =>
-{
-    client.BaseAddress = new Uri("https://localhost:7258/Consumer/");
-});
-
-// Register the GridRequester as a hosted service
-builder.Services.AddHostedService<GridRequester>();
-
-
-
 // Add logging
 builder.Logging.ClearProviders();
 builder.Logging.AddConsole();
