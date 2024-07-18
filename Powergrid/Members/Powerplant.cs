e11d75bcea6b66334000f0b9c5401edc2511b37e
Fabian Praxmarer <fapraxmarer23@gmail.com>
2024-07-18 14:00:38 +0200

    Added Endpoints for Charts | Deleted Http Requester

--- Powergrid/Members/Powerplant.cs
+++ Powergrid/Members/Powerplant.cs
@@ -3,6 +3,8 @@
     public double Energy { get; } = 100;
     public string Name { get; set; }
 
+    public double Produced { get; set; }
+
     public Powerplant(string name)
     {
         this.Name = name;
